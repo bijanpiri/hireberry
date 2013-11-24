9a9a3e87473b0e8476b3c84e4722fd201b864988
coybit <coybit@gmail.com>
2013-11-24 15:21:09 +0330

    a little change

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ passport.use(new TwitterStrategy({
   	//parseApp.insert('twitter', { foo: token }, function (err, response) {
   	//	console.log(response);
 	//});
-      console.log('$$$$$$$$$$$$$$$[' + req.body.uid + ']$$$$$$$$$$$$$$$' );
+      console.log('$$$$$$$$$$$$$$$[' + req.uid + ']$$$$$$$$$$$$$$$' );
     return done(null, profile);
   }));
 
