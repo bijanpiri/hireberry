3326ff210449f3245a9823be201eaede44479aa3
bijan <bijbij@gmail.com>
2013-12-19 15:48:50 +0330

    conflict solved

--- public/script/newboard.js
+++ public/script/newboard.js
@@ -24,16 +24,22 @@ function createBoard(){
 function privacyChanged(){
     $('#privacy').val($(this).text());
 }
+var clickTimeout;
+var clickDetected = true;
+
 function main(){
 
     fillCategory();
-<<<<<<< HEAD
+
     $('div.btn-group>button').click(privacyChanged);
     $('button#createBoard').click(createBoard);
     var map = L.mapbox.map('map', 'coybit.gj1c3kom').setView([37.9, -77],4);
-=======
-    var clickTimeout;
-    var clickDetected = true;
+
+    var marker = L.marker(new L.LatLng(37.9, -77), {
+        icon: L.mapbox.marker.icon({'marker-color': 'CC0033'}),
+        draggable: true
+    });
+
 
     var map = L.mapbox.map('map', 'coybit.gj1c3kom',{
         doubleClickZoom: false
@@ -51,31 +57,10 @@ function main(){
                     marker.setLatLng(e.latlng);
             },250);
         });
->>>>>>> 7de29c72c4c227497fa5969ac4078d7aa2629c87
 
-    var marker = L.marker(new L.LatLng(37.9, -77), {
-        icon: L.mapbox.marker.icon({'marker-color': 'CC0033'}),
-        draggable: true
-<<<<<<< HEAD
-    });
-    map.doubleClickZoom.disable();
 
-    map.on('click dblclick',
-        function(e) {
-            if(e.type=='click')
-                marker.setLatLng(e.latlng);
-            else
-                map.setView(e.latlng, map.getZoom() + 1);
-
-        });
-
-//    marker.bindPopup('Locate your board in map');
-=======
-    }).addTo(map);
-
-    /*
     marker.bindPopup('Locate your board in map');
->>>>>>> 7de29c72c4c227497fa5969ac4078d7aa2629c87
+
     marker.on('drag',
         function(e){
             var latlng=marker.getLatLng();
@@ -83,6 +68,4 @@ function main(){
             $('#lng')[0].value=(JSON.stringify( latlng.lng));
         }
     );
-    */
-
 }
