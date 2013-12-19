c6dc4640723977eaf690bf3c00b21fd87b500d8a
bijan <bijbij@gmail.com>
2013-12-19 19:30:31 +0330

    added geolocatin

--- public/script/newboard.js
+++ public/script/newboard.js
@@ -30,7 +30,18 @@ function mapEvent(e){
 }
 var clickTimeout;
 var clickDetected = true;
+var map;
+function locate(){
+    if (!navigator.geolocation) {
+        locationError('Finding Location is not supported by your browser.');
+    } else {
 
+        map.locate();
+
+    }
+
+}
+var marker;
 function main(){
 
     fillCategory();
@@ -39,27 +50,28 @@ function main(){
     $('button#createBoard').click(createBoard);
     $('#map').dblclick(mapEvent);
     $('#map').click(mapEvent);
-    var map = L.mapbox.map('map', 'coybit.gj1c3kom',{
+    $('#locate').click( locate);
+    map = L.mapbox.map('map', 'coybit.gj1c3kom',{
         doubleClickZoom: false
         })
         .setView([37.9, -77],4)
         .on('dblclick', function(e) {
-//            alert('double clicked map');
             clickDetected = false;
             clearTimeout(clickTimeout);
             map.setView(e.latlng, map.getZoom() + 1);
+            e.preventDefault();
+            e.stopPropagation();
         })
         .on('click', function(e) {
             if(eventType='click');
-
-//            clickDetected = true;
-//            clickTimeout = setTimeout(function(){
-//                if(clickDetected)
+            clickDetected = true;
+            clickTimeout = setTimeout(function(){
+                if(clickDetected)
                     marker.setLatLng(e.latlng);
-//            },250);
+            },400);
         });
 
-    var marker = L.marker(new L.LatLng(37.9, -77), {
+    marker = L.marker(new L.LatLng(37.9, -77), {
         icon: L.mapbox.marker.icon({'marker-color': 'CC0033'}),
         draggable: true
 
@@ -72,6 +84,33 @@ function main(){
             $('#lng')[0].value=(JSON.stringify( latlng.lng));
         }
     ).addTo(map);
+
+    locate();
+    map.on('locationfound', locationFound);
+
+    map.on('locationerror', locationError);
+}
+//function messageClass(klass){
+//    var msg=$('#message');
+//    msg.removeClass('alert-info');
+//    msg.removeClass('alert-success');
+//    msg.removeClass('alert-error');
+//    msg.addClass(klass);
+//}
+function locationError(msg) {
+//    if(msg==null)
+//        msg='Failed to find location. Allow your browser to access your location.';
+    $('#msgLocationError').text(msg.message).show();
+}
+
+function ErrorMsg(msg) {
+    $('.message-error').show();
+    $('.message-error>.msg-error').text(msg);
+}
+
+function locationFound(e) {
+    map.fitBounds(e.bounds);
+    marker.setLatLng(e.latlng);
 
 
 }
