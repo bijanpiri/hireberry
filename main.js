eb9cfafa552a6f204e03726e07c74eee7c1c32b6
coybit <coybit@gmail.com>
2013-12-19 17:56:04 +0330

    show more log

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ everyauth.google
 
         var promise = this.Promise();
 
-        console.log('Finding User ...');
+        console.log('Finding User ...' + googleUserMetadata);
 
         BUsers.findOne({googleid:googleUserMetadata.id}, function(err,user){
 
