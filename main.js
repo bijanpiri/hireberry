14414ed143d600653a76f0528c58b076c5a095b0
coybit <coybit@gmail.com>
2013-12-22 09:34:19 +0330

    First Google Analytic Event Tracking code is added. alse Port 80->5000

--- main.js
+++ main.js
@@ -218,7 +218,7 @@ app.configure(function() {
 
 /************** Starting Server ****************/
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
