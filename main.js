2d819f509400f2f2cd4d0974ef0e1dee7b3730df
coybit <coybit@gmail.com>
2013-12-22 09:14:36 +0330

    Google Analytic is added

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ app.configure(function() {
 
 /************** Starting Server ****************/
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
