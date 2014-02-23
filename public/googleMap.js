400de83ffc9561c95e095d0aad4a607e63c2f0e2
DorsaPiri <dorsapiri@gmail.com>
2014-02-23 10:19:43 +0330

    convert mapbox to gmap in profile

--- public/googleMap.js
+++ public/googleMap.js
@@ -5,31 +5,69 @@
 $.fn.googleMaps = function(){
 
     var map;
-
-    /*function loadScript(){
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = 'http://maps.googleapis.com/maps/api/js?key=AIzaSyB9JJKEbOztuSVauWEU58UeQJWtuzip2eU&sensor=false'
-            +'&callback=initialize'
-        ;
-        document.body.appendChild(script);
-//
-//        document.getElementsByTagName('head')[0].appendChild(script);
-    }*/
-
+    var mycenter;
+    var boards = [];
+    var markers;
     function initialize(){
+
 
         var map_property = {
             center: new google.maps.LatLng(51.508742,-0.120850),
             zoom: 5,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        map = new google.maps.Map(document.getElementById('map-canvas'),map_property);
+    map = new google.maps.Map(document.getElementById('map-canvas'),map_property);
+    //addMarker();
 
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
-//    window.onload=loadScript;
 
+    function addMarker(){
+        mycenter = new google.maps.LatLng(51.508742,-0.120850);
+        var marker = new google.maps.Marker({
+            position: mycenter
+        });
+   // marker.setMap(map);
+    }
+    function getLocation(){
+        markers = new MarkerManager(map);
+        $.getJSON('/board/get/public',function(publicBoards){
+            $.each(publicBoards,function(index,board){
+              if(board.locationlat && board.locationlng){
+                  boards.push(board);
+                  markers = new google.maps.Marker({
+                      type:'Feature',
+                      position: new google.maps.LatLng(board.locationlng,board.locationlat),
+                      map: map,
+                      'result':[{"address_components":[{
+                          'title':board.name,
+                          'boardIndex':index,
+                          'description':board.category,
+                          'icon':{
+                              'size':new google.maps.Size(32,32),
+                              'color':'522'
+                          }
+
+                          }]
+                      }]
+
+                  });
+                  markers.addMarker(marker,0);
+              }
+            });
+            markers.refresh();
+           // $('#map-canvas').gmap('addBounds',marker.position)
+            // continu json
+            try{
+                markers.setMap(map);
+            }catch (e){}
+
+        });
+
+    }
+
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    getLocation();
     this.attr('id','map-canvas');
     return this;
 
