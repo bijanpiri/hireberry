5e1c1375a85291bad508e4e78551541a614b2c3b
coybit <coybit@gmail.com>
2014-03-18 11:52:32 +0330

    color selection is added to separator

--- public/script/Widgets/SeperatorWidget.js
+++ public/script/Widgets/SeperatorWidget.js
@@ -13,9 +13,18 @@ function SeperatorWidget(){
 
     this.widgetDidAdd = function() {
         this.setToolbar('.toolbar-separatorWidget');
+
         this.toolbar.find('input[name=text]').keyup( (function(layout) {
             return function(e) {
                 layout.find('.flatSection span').text( $(e.target).val() );
+            }
+        })(this.layout))
+
+        this.toolbar.find('input[type=color]').change( (function(layout) {
+            return function(e) {
+                var color = $(e.target).val();
+                layout.find('.flatSection span').css( 'color', color);
+                layout.find('.flatSection hr').css( 'border-color', color);
             }
         })(this.layout))
     }
