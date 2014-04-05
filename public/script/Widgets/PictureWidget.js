d3eb3c0975c7036386e5638fc0a0da9abb2f4c51
Bijan <bijbij@gmail.com>
2014-04-05 13:41:19 +0430

    Fixed add/change and edit toolbar button for image widget

--- public/script/Widgets/PictureWidget.js
+++ public/script/Widgets/PictureWidget.js
@@ -2,65 +2,26 @@ function PictureWidget(){
     Widget.call(this);
 
     var layout = "";
-    var jcropApi;
-    var coordinates;
-    var imgly = new imglyKit({container: "#imageEditor"});
+
+    var imgEditor;
+
     function initLayout1() {
         layout = this.clone('.image-widget');
 
         var fileInput = layout.find('input[type=file]').change(
 
         );
-        var imgEditor=$('.image-editor-dialog');
         layout.find('button').click(function(){
             fileInput.click();
         });
-
-
-
-
+        imgEditor=layout.find('.image-editor-dialog');
         if(editMode){
             fileInput.fileupload({
                 url:'/flyer/upload',
                 dataType: 'json',
                 replaceFileInput:false,
-                add:function(e,dudata){
-                    imgEditor.show();
-                    var reader = new FileReader();
-                    reader.onload =
-                        function (progress) {
-//                            try {
-                                imgEditor.find('.imgly-container').empty();
-                                var d=progress.target.result;
-                                imgly.run(d);
-                                imgEditor.find('.image-edit-cancel').click(function(){
-                                    imgEditor.hide();
-                                });
-                                imgEditor.find('.image-edit-ok').click(function(){
-                                    imgEditor.hide();
-                                    imgly.renderToDataURL("image/jpeg", function (err, dataurl){
-                                        var blob=dataURLtoBlob(dataurl);
-//                                        dudata.files=[];
-                                        dudata.files[0]=blob;
-//                                        fileInput.fileupload('option','formData',undefined);
-                                        dudata.submit();
-                                    });
-                                });
-
-//                            } catch (ex) {
-//                                console.log(ex);
-//                                imgEditor.hide();
-//
-//                                if(ex.name == "NoSupportError") {
-//                                    alert("Your browser does not support canvas.");
-//                                } else if(ex.name == "InvalidError") {
-//                                    alert("The given file is not an image");
-//                                }
-//                            }
-                        };
-
-                    reader.readAsDataURL(dudata.files[0]);
-                },
+                add:add,
+                dropZone:layout,
                 done: function (e, data) {
                     layout.find('.imageWidgetInnerContainer').hide();
                     var img=layout.find('img');
@@ -71,10 +32,46 @@ function PictureWidget(){
                     console.log(progress);
                 }
             });
-            fileInput.fileupload('option',{dropZone:layout});
+
         } else {
             this.portlet.find('input[type=file]').remove();
         }
+    }
+    function add(e,dudata){
+        imgEditor.show();
+        var reader = new FileReader();
+        reader.onload =
+            function (progress) {
+                try {
+                    var imgly = new imglyKit({container: layout.find(".image-dialog-content")});
+                    imgEditor.find('.imgly-container').empty();
+                    var d=progress.target.result;
+                    imgly.run(d);
+                    imgEditor.find('.image-edit-cancel').click(function(){
+                        imgEditor.hide();
+                    });
+                    imgEditor.find('.image-edit-ok').click(function(){
+                        imgEditor.hide();
+                        imgly.renderToDataURL("image/jpeg", function (err, dataurl){
+                            var blob=dataURLtoBlob(dataurl);
+                            dudata.files[0]=blob;
+                            dudata.submit();
+                        });
+                    });
+
+                } catch (ex) {
+                    console.log(ex);
+                    imgEditor.hide();
+
+                    if(ex.name == "NoSupportError") {
+                        alert("Your browser does not support canvas.");
+                    } else if(ex.name == "InvalidError") {
+                        alert("The given file is not an image");
+                    }
+                }
+            };
+
+        reader.readAsDataURL(dudata.files[0]);
     }
 
     initLayout1.call(this);
@@ -88,39 +85,9 @@ function PictureWidget(){
         this.addToolbarCommand('add',function(){
             layout.find('input[type=file]').click();
         }).addToolbarCommand('edit',function(){
-                event.preventDefault();
-                var img=layout.find('img');
-                img.Jcrop({
-                    onSelect: function (coords) {
-                        coordinates = coords;
-                    },
-                    onRelease: function () {
-                        coordinates = null;
-                    }
-                },function(){jcropApi=this;});
+              imgEditor.show();
 
-                widget.toolbar.find('[command=crop]').show();
-                widget.toolbar.find('[command=edit]').hide();
-
-            }).addToolbarCommand('crop',function(){
-                event.preventDefault();
-                widget.toolbar.find('[command=crop]').hide();
-                widget.toolbar.find('[command=edit]').show();
-
-                var img=layout.find('img');
-                if(img && coordinates){
-                    replaceResults(loadImage.scale(img[0], {
-                        left: coordinates.x,
-                        top: coordinates.y,
-                        sourceWidth: coordinates.w,
-                        sourceHeight: coordinates.h,
-                        minWidth: img.width()
-                    }));
-                    coordinates = null;
-                }
-                if(jcropApi)
-                    jcropApi.destroy();
-            });
+        });
 
     }
 
