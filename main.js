5fd43d7bbf8e3619bb07349061e0675ed14e1515
coybit <coybit@gmail.com>
2014-04-16 12:09:52 +0430

    a simple application preview winow is added to dashboard

--- main.js
+++ main.js
@@ -560,6 +560,13 @@ app.get('/api/forms', routerDashboard.forms );
 app.get('/api/applications', routerDashboard.applications );
 app.post('/api/applications/:applicationID', routerDashboard.updateApplication );
 app.get('/api/applications/stat', routerDashboard.statisticalInfo )
+app.get('/api/application/:appID', function(req,res) {
+    BApplications.find( {_id:req.params.appID} , function(err,application) {
+        if( err )
+            return res.send(306)
+        res.send(application);
+    })
+})
 //endregion
 
 // region Flyers
