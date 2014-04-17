3e185a07b45b8e8ead24498e901acfa2051b8d23
coybit <coybit@gmail.com>
2014-04-17 17:54:07 +0430

    Skill Widget is improve (exteremly!)

--- public/script/Widgets/SkillWidget.js
+++ public/script/Widgets/SkillWidget.js
@@ -2,24 +2,104 @@ function SkillWidget(){
     Widget.call(this);
 
     var layout = '';
+    var skillsButtons = {
+        'jquery' : ['jquery','jQuery', '#1C283B', '#151E2C', '0 -214px', '-58px -214px', 57, 54, 100],
+        'html5' : ['html5','HTML5', '#0070BB', '#00548C', '0 -48px', '-51px -48px', 50, 55, 100],
+        'css3' : ['css3','CSS3', '#E44D26', '#AB3A1C', '0 -103px', '-61px -103px', 61, 55, 100],
+        'javascript' : ['javascript','Javascript', '#F8DC3D', '#BAA52E', '0 -158px', '-77px -158px', 77, 56, 100],
+        'less' : ['less','Less', '#315AA0', '#254378', '0 -269px', '-136px -269px', 136, 60, 150],
+        'bootstrap' : ['bootstrap','Bootstrap', '#5A4080', '#433060', '-47px -329px', '-47px -329px', 47, 55, 100],
+        'git' : ['git','Git', '#F05133', '#B43D26', '0 -384px', '-62px -384px', 62, 61, 100],
+        'mongodb' : ['mongodb','Mongodb', '#5E3E2D', '#462E22', '0 -447px', '-34px -447px', 35, 72, 100],
+        'nodejs' : ['nodejs','Nodejs', '#8FC440', '#6B9330', '0 -518px', '-57px -518px', 57, 60, 100]
+    };
 
+    function buildButton(id, title,
+                         activeBackColor,
+                         activeShadowColor,
+                         activeIconPosition,
+                         passiveIconPosition,
+                         iconWidth,
+                         iconHeight,
+                         buttonWidth) {
 
-    function updateSkillButtons(portlet, chosenBox) {
-        var skills = chosenBox.val() || [];
+        // Build structure
+        var beautifulButtonObj = $('<div>')
+            .addClass('beautifulButton');
 
-        if( skills.length == 0 ) { // Empty-State
-            portlet.find('.emptyState').show();
-        }
-        else {
-            portlet.find('.emptyState').hide();
-        }
+        var inputObj = $('<input>').attr('type','checkbox')
+            .attr('id',id)
+            .attr('name','skill')
+            .val(id);
+        var labelObj =$('<label>').attr('for',id);
+        var iconObj = $('<i>').addClass(id);
+        var pObj = $('<p>').text(title);
+        var removeBtnObj = $('<span>')
+            .addClass('beautifulButtonRemove')
+            .text('X')
+            .click( function() {
+                beautifulButtonObj.remove();
+            });
 
-        portlet.find('input[name=skill]').each(function(i,input){
-            input.parentElement.style.display= skills.indexOf(input.value)>=0 ?
-                '':'none';
+        labelObj.append( iconObj );
+        beautifulButtonObj.append(inputObj)
+            .append(labelObj)
+            .append(pObj)
+
+        if( this.editMode )
+            beautifulButtonObj.append(removeBtnObj);
+
+        // Customize it
+        labelObj.width(buttonWidth || 100);
+        iconObj.css('background-position',passiveIconPosition)
+            .width(iconWidth)
+            .height(iconHeight);
+
+        inputObj.change( function() {
+            changeState( isChecked() );
+        });
+        labelObj.mouseenter( function() {
+            var currentBoxShadow = labelObj.css('box-shadow').replace(/^.*(rgba?\([^)]+\)).*$/,'$1');
+
+            labelObj.css('-webkit-box-shadow','0 3px ' + currentBoxShadow);
+            labelObj.css('box-shadow','0 3px ' + currentBoxShadow);
+        });
+        labelObj.mouseleave( function() {
+            var currentBoxShadow = labelObj.css('box-shadow').replace(/^.*(rgba?\([^)]+\)).*$/,'$1');
+
+            labelObj.css('-webkit-box-shadow','0 4px ' + currentBoxShadow);
+            labelObj.css('box-shadow','0 4px ' + currentBoxShadow);
         });
 
+        function isChecked() { return beautifulButtonObj.find('input').is(':checked') }
 
+        function changeState(turnOn) {
+            if( turnOn ) {
+                labelObj.css('background-color', activeBackColor);
+                labelObj.css('-webkit-box-shadow','0 3px ' + activeShadowColor);
+                labelObj.css('box-shadow','0 3px ' + activeShadowColor);
+
+                iconObj.css('background-position', activeIconPosition)
+            } else {
+                labelObj.css('background-color', '');
+                labelObj.css('-webkit-box-shadow','');
+                labelObj.css('box-shadow','');
+
+                iconObj.css('background-position',passiveIconPosition)
+            }
+        }
+
+        return beautifulButtonObj;
+    }
+
+
+    function addSkillButton(id) {
+
+        if( this.portlet.find('.skillsButtons input[name=skill][id=' + id + ']').length == 0 ) {// Check for repetition
+            var skillButton = skillsButtons[ id ];
+            var btn =  buildButton.apply(this,skillButton);
+            this.portlet.find('.skillsButtons').append( btn );
+        }
     }
 
     function initLayout() {
@@ -31,47 +111,47 @@ function SkillWidget(){
 
     this.widgetDidAdd = function() {
 
+        var availableTags = [];
+
+        for( var skill in skillsButtons )
+            availableTags.push(skill);
+
         this.setToolbar('.toolbar-skillsWidget');
-        var chosenBox = this.toolbar.find('.chosen-select');
 
-        chosenBox.chosen({
-            width:'500px',
-            no_results_text: "Oops, nothing found!"
-        }).on('change', (function(portlet,chosenBox) {
-                return function() {
-                    updateSkillButtons( portlet,  chosenBox );
-                };
-            })(this.portlet, chosenBox));
+        var widget = this;
 
-        // Initialize
-        updateSkillButtons( this.portlet,  chosenBox );
+        this.portlet.find('input[name=newSkillTextbox]').keyup( function(e) {
+            if( e.keyCode == 13 ) {
+
+                var skill = $(this).val().trim().replace(' ','').toLowerCase();
+                $(this).val('')
+                addSkillButton.call(widget,skill);
+            }
+        }).autocomplete({
+                source: availableTags
+            });
+
+        if( this.editMode===false ) {
+            this.portlet.find('.skillAddButton').hide();
+            this.portlet.find('.beautifulButtonRemove').hide();
+        }
     }
 
     this.serialize = function() {
-        var data={
-            chosen:this.toolbar.find('.toolbar-skillsWidget select').serialize().replace(/chosen=/gi,'').split('&'),
-            skills:this.portlet.find('.skillWidget input').serialize().replace(/skill=/gi,'').split('&')
-        };
+        var data = [];
+        var skillButtons = this.portlet.find('.skillsButtons .beautifulButton input[name=skill]');
+
+        for( var i=0; i<skillButtons.length; i++ )
+            data.push( $(skillButtons[i]).attr('id') );
+
+
         return data;
     }
 
     this.deserialize = function( data ) {
 
-        var chosen=this.toolbar
-            .find('.chosen-select')
-            .val(data.chosen)
-            .trigger("chosen:updated");
-
-        var skills = data.skills || [];
-
-        this.portlet.find('.skillWidget input[name=skill]').each(
-            function(i,input){
-                $(input).prop('checked',skills.indexOf(input.value)>=0).change();
-            }
-        );
-
-        updateSkillButtons(this.portlet,chosen);
-
+        for( var i=0; i<data.length; i++ )
+            addSkillButton.call(this,data[i]);
     };
 }
 SkillWidget.prototype=new Widget();
